7749acfffdde288f432c060521a2c1c1a9bccb05
mjamontano <147313942+mjamontano@users.noreply.github.com>
2024-07-05 17:47:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,16 @@ closeButtons.forEach(function(closeButton) {
     modal.style.display = "none";
   });
 });
+
+// Full Images
+var fullImgBox = document.getElementById("fullImgBox");
+var fullImg = document.getElementById("fullImg");
+
+function openFullImg(pic){
+  fullImgBox.style.display = "flex";
+  fullImg.src = pic;
+}
+
+function closeFullImg(){
+  fullImgBox.style.display = "none";
+}
